3bf8c3ee5af79d8511d0c1f7709f9c3079fde907
Anton Lisovski <lisovskijj.99@gmail.com>
2023-05-23 18:11:34 +0300

    init()

---
+++ src/main/java/by/EMERCOM/EmercomApplication.java
@@ -0,0 +1,13 @@
+package by.EMERCOM;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmercomApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmercomApplication.class, args);
+	}
+
+}
---
+++ src/test/java/by/EMERCOM/EmercomApplicationTests.java
@@ -0,0 +1,13 @@
+package by.EMERCOM;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmercomApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
